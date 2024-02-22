00049af0e29d615b3d0d6364a8fb16c785684b7f
Mohitxb <tumsarcafe@gmail.com>
2024-02-22 12:40:53 +0530

    update counter priject

---
+++ First 10 Project/Counter/script.js
@@ -0,0 +1,41 @@
+let c = document.getElementById("count");
+let d = document.getElementById("check");
+
+let count = 0;
+
+function incr() {
+   if ( count < 20){
+    count++
+   }else{
+    alert("Limit Reached")
+   }
+    c.textContent = count
+    check()
+}
+
+function decr() {
+    if ( count > 0 ){
+        count--;
+    }
+    else{
+        count= 0;
+    }
+   
+    c.textContent = count;
+    check()
+}
+function reset() {
+    count = 0
+    c.textContent = count;
+    d.style.color = "Black"
+}
+
+function check(){
+ if ( count%2==0){
+    d.textContent = "Even"
+    d.style.color = "Green"
+ }else{
+    d.textContent ="Odd"
+    d.style.color = "Red"
+ }
+}
